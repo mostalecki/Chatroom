4b002cf31cec9ce610a810040bb18fe9a9b84802
mostalecki <1b13ostalecki@gmail.com>
2020-02-17 01:19:33 +0100

    Add active room list

--- chat/views.py
+++ chat/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Room, Connection
 
 # Create your views here.
 
 def homepage(request):
     ''' Main page '''
-
-    return render(request, 'chat/home.html')
+    rooms = Room.objects.filter(is_private=False)
+    return render(request, 'chat/home.html', {'rooms':rooms})
 
 def room(request, room_name):
     ''' Page of a chatroom'''
