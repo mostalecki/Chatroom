5f38e0f89396e72bb008c6cf126218c058cb3f44
mateusz <1b13ostalecki@gmail.com>
2021-02-11 21:15:11 +0100

    Fix naming issues after splitting User and Profile apps

--- backend/src/apps/profile/models.py
+++ backend/src/apps/profile/models.py
@@ -3,6 +3,5 @@ from src.apps.authentication.models import User
 
 
 class Profile(models.Model):
-
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     avatar = models.ImageField(upload_to="avatars/", default="avatars/default.png")
