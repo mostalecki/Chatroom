9e32f026398b321e9061d61b62a98de547eada43
mateusz <1b13ostalecki@gmail.com>
2021-03-08 19:38:05 +0100

    Add endpoint with current user's info

--- backend/src/apps/authentication/serializers.py
+++ backend/src/apps/authentication/serializers.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
 
+from src.apps.authentication.models import User
+
 
 class UserRegisterSerializer(serializers.Serializer):
     email = serializers.EmailField()
     username = serializers.CharField(max_length=255)
     password = serializers.CharField(max_length=255)
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("username", "email")
 
 
 class TokenSerializer(serializers.Serializer):
