62d18e35f78e934d22fb8b0628aedeeb34d70bf1
mateusz <1b13ostalecki@gmail.com>
2021-04-29 14:46:33 +0200

    Change room create response serializer

--- backend/src/apps/chat/views.py
+++ backend/src/apps/chat/views.py
@@ -7,7 +7,11 @@ from rest_framework import status
 from drf_yasg.utils import swagger_auto_schema
 
 from src.apps.chat.models import Room
-from src.apps.chat.serializers import RoomSerializer, RoomCreateSerializer
+from src.apps.chat.serializers import (
+    RoomSerializer,
+    RoomCreateInputSerializer,
+    RoomCreateOutputSerializer,
+)
 from src.utils.mixins import ExceptionHandlerMixin
 from src.apps.chat.services import room_create
 
@@ -19,26 +23,30 @@ class RoomViewSet(
     ListModelMixin,
     RetrieveModelMixin,
 ):
-    queryset = Room.objects.select_related("owner").with_user_count().filter(is_active=True).order_by("-users_count")
+    queryset = (
+        Room.objects.select_related("owner").with_user_count().order_by("-users_count")
+    )
     permission_classes = (AllowAny,)
     serializer_class = RoomSerializer
     pagination_class = LimitOffsetPagination
 
     def filter_queryset(self, queryset):
         if self.action == "list":
-            queryset = queryset.filter(is_private=False)
+            queryset = queryset.filter(is_private=False, is_active=True)
         return super().filter_queryset(queryset)
 
     @swagger_auto_schema(
-        request_body=RoomCreateSerializer,
-        responses={status.HTTP_201_CREATED: RoomSerializer},
+        request_body=RoomCreateInputSerializer,
+        responses={status.HTTP_201_CREATED: RoomCreateOutputSerializer},
     )
     def create(self, request, *args, **kwargs):
-        serializer = RoomCreateSerializer(data=request.data)
+        serializer = RoomCreateInputSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
         owner = request.user if request.user.is_authenticated else None
 
         room = room_create(**serializer.validated_data, owner=owner)
 
-        return Response(data=RoomSerializer(room).data, status=status.HTTP_201_CREATED)
+        return Response(
+            data=RoomCreateOutputSerializer(room).data, status=status.HTTP_201_CREATED
+        )
